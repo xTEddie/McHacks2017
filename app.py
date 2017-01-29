77351557d34e0d734e68953ba2f1096f05d7f96c
Soqueen <sokhenglim973@gmail.com>
2017-01-28 22:24:02 -0500

    convert to dictionary time

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import time
-
-
 from flask import Flask, redirect, request, render_template, jsonify
 
 from youtube_transcriber import search_keywords
@@ -13,31 +10,28 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/search_keyword',  methods=['POST'])
+@app.route('/search_keyword', methods=['POST'])
 def searchKeyWord():
     """
     Expected receiving the keyWord and youtubeURL.
-    :return:
+    :return: None
     """
 
     url = request.form["url"]
     keyword = request.form["keyword"]
-
-    time_stamps = timeStamp(search_keywords(url, keyword))
-    print(time_stamps)
-    return "duma"
+    timeStamp(search_keywords(url, keyword))
 
 
 def timeStamp(list_time):
-    format_time = list()
+    format_time = dict()
+    i = 0
     for time in list_time:
         m, s = divmod(time, 60)
         h, m = divmod(m, 60)
-        format_time.append("%d:%02d:%02d" % (h, m, s))
+        format_time[str(i)] = "%dh:%02dm:%02ds" % (h, m, s)
+        i += 1
     return format_time
+
 
 if __name__ == '__main__':
     app.run()
-
-
-
