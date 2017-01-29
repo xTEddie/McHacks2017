1dfefe0c901ac0c246f1c3f57d011071541a536d
xTEddie <t.eddie@live.com>
2017-01-29 00:14:57 -0500

    Add logic for timestamp buttons

--- static/js/style.js
+++ static/js/style.js
@@ -31,8 +31,34 @@ $(document).ready(function() {
                 "keyword": keyword
             },
             success: function(data){
-                // Write logic to render buttons for time
-                console.log(data);
+                // Write logic to render buttons for time                
+                var results = $("#results");
+
+                var data_size = Object.keys(data).length;                
+
+                if(data_size === 0){
+                    results.html(""); // Delete content in table
+                    return;
+                }
+
+                var output = "";
+                
+                $.each(data, function(index, value){   
+                    var key = Object.keys(value)[0];                                     
+                    output += "<button type='button' class='btn btn-default timestamp' value='" + value[key] + "'>" + key + "</button>"; 
+                    
+                    if((parseInt(index) + 1) % 5 == 0){
+                        output += "<br>";
+                    }
+                });
+
+                results.html(output);
+
+                $(".timestamp").click(function(){      
+                    var value = $(this).attr("value");                                                  
+                    var new_url = url + "?start=" + parseInt(value);
+                    $("#embedded-video").attr("src", new_url);
+                });
             }
         });
 
